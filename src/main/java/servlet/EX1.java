c170be5f8b407f2bae1a4213ff1d3c8b5e174292
ke8190 <samrai753@gmail.com>
2024-10-18 13:49:22 +0900

    change to java21 from java17

---
+++ src/main/java/servlet/EX1.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/ex1")
+public class EX1 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Hello </body></html>");
+	}
+
+}
